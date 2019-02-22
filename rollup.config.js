8bb164e737d47100f886445a74d15e1b1a8dc4a7
XKH <xkh@XKHdeMacBook-Pro.local>
2019-02-22 18:40:25 +0800

    deploy

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pkg from './package.json';
+import {terser} from 'rollup-plugin-terser';
 
 export default [
 	// browser-friendly UMD build
@@ -13,7 +14,8 @@ export default [
 		},
 		plugins: [
 			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			commonjs(), // so Rollup can convert `ms` to an ES module
+            terser()
 		]
 	},
 
