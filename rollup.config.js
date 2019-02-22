072ff991d77f793112fc601592851c0b84e28c4d
XKH <xkh@XKHdeMacBook-Pro.local>
2019-02-22 17:25:43 +0800

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default {
     input: 'src/main.js',
     output: {
         file: process.env.NODE_ENV === "production"?'lib/rollup.min.js':'lib/rollup.js',
-        format: 'cjs',
+        format: 'umd',
         name: 'xkh'
     },
     plugins: [
